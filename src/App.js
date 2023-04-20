4abbafacf6e2e52d127c4e5c041ad6683943bb7e
Vishal Rathore <vishal.rathore@multitech.com>
2023-04-20 01:00:43 -0500

    added basic filtering for monsters, using useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [name, setName] = useState("Bruce Wayne");
+  const [monsters, setMonsters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [apiURL] = useState("https://jsonplaceholder.typicode.com/users");
+  const [input, setInput] = useState("");
+  const [showData, setShowData] = useState(monsters);
 
-  function handleClick() {
-    setName("Tony Start");
-    console.log(name);
-  }
+  useEffect(() => {
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((users) => {
+        setMonsters(users);
+        setLoading(false);
+      });
+  }, [apiURL]);
+
+  useEffect(() => {
+    const matches = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(input.toLowerCase())
+    );
+    setShowData(matches.length ? matches : []);
+  }, [input, monsters]);
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hi, I'm {name}. I work at MTS</p>
-        <button onClick={handleClick}>Click Me</button>
-      </header>
+      {loading ? (
+        <p>Loading... Please wait.</p>
+      ) : (
+        <>
+          <input
+            className='search-box'
+            type='search'
+            placeholder='Search monsters'
+            value={input}
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
+          />
+          {showData.length <= 0 ? (
+            <p>No data found...</p>
+          ) : (
+            showData.map((monster) => <p key={monster.id}>{monster.name}</p>)
+          )}
+        </>
+      )}
     </div>
   );
 }
